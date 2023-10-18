e3a7e88d675c3e391ea02f71c6a5fc84cf4d044c
abraham23o <abraonyonka23@gmail.com>
2023-10-18 21:59:49 +0300

    custom printf refactor

--- _printf.c
+++ _printf.c
@@ -1,61 +1,25 @@
-#include "main.h" 
- /** 
-  * _printf - user-defined printf 
-  * @format: a char string 
-  * Return: number of characters printed excluding null char used 
-  * to end strings 
-  */ 
- int _printf(const char *format, ...) 
- { 
-         int x = 0, i = 0, count; 
-         char ch, *str; 
-         va_list args; 
-  
-         va_start(args, format); 
-  
-         while (*format) 
-         { 
-                 if (*format != '%') 
-                 { 
-                         write(1, format, 1); 
-                         count++; 
-                 } 
-                 else 
-                 { 
-                         format++; 
-                         if (*format == '\0') 
-                                 break; 
-  
-                         if (*format == '%') 
-                         { 
-                                 write(1, format, 1); 
-                                 count++; 
-                         } 
-                         else if (*format == 'c') 
-                         { 
-                                 ch = va_arg(args, int); 
-                                 write(1, &ch, 1); 
-                                 count++; 
-                         } 
-                         else if (*format == 's') 
-                         { 
-                                 str = va_arg(args, char *); 
-  
-                                 while (str[i] != '\0') 
-                                         i++; 
-  
-                                 write(1, str, i); 
-                                 count += i; 
-                         } 
-                         else if (*format == 'd') 
-                         { 
-                                 x = va_arg(args, int); 
-                                 count++; 
-                         } 
-                 } 
-                 format++; 
-         } 
-         va_end(args); 
-         return (count); 
- }
+#include "main.h"
+/**
+ * _printf - print anything
+ * @format: string to be printed
+ * Return: the number of characters printed (excluding the null
+ * byte used to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int count = 0;
 
+	va_start(ap, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+			count += print_format(*(++format), ap);
+		else
+			count += write(1, format, 1);
+		++format;
+	}
+	va_end(ap);
+	return (count);
+}
